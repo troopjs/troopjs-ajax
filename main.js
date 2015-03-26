eb9c41b32418a3662c36bb8e7223d34a262adbdc
Mikael Karon <mikael@karon.se>
2015-03-26 10:59:12 +0800

    Release 1.0.0-rc.2

--- main.js
+++ main.js
@@ -4,46 +4,46 @@ define('troopjs-ajax/version',[], { 'toString': function () { return ; } });
  * @license MIT http://troopjs.mit-license.org/
  */
 define('troopjs-ajax/config',[
-	"module",
-	"troopjs-core/config",
-	"mu-merge/main"
+  "module",
+  "troopjs-core/config",
+  "mu-merge/main"
 ], function (module, config, merge) {
-	
+  
 
-	/**
-	 * @class ajax.config.ajax
-	 * @private
-	 */
-	var AJAX = {
-		/**
-		 * Method that generates default settings which will be added to any ajax request
-		 * @return {Object}
-		 */
-		"settings" : function () {
-			return {
-				"headers": {
-					"x-troopjs-request-id": new Date().getTime()
-				}
-			}
-		}
-	};
+  /**
+   * @class ajax.config.ajax
+   * @private
+   */
+  var AJAX = {
+    /**
+     * Method that generates default settings which will be added to any ajax request
+     * @return {Object}
+     */
+    "settings": function () {
+      return {
+        "headers": {
+          "x-troopjs-request-id": new Date().getTime()
+        }
+      };
+    }
+  };
 
-	/**
-	 * Provides configuration for the ajax service
-	 * @class ajax.config
-	 * @extends core.config
-	 * @private
-	 * @alias feature.config
-	 */
+  /**
+   * Provides configuration for the ajax service
+   * @class ajax.config
+   * @extends core.config
+   * @private
+   * @alias feature.config
+   */
 
-	return merge.call({}, config, {
-		/**
-		 * Ajax configuration
-		 * @cfg {ajax.config.ajax}
-		 * @protected
-		 */
-		"ajax": AJAX
-	}, module.config());
+  return merge.call({}, config, {
+    /**
+     * Ajax configuration
+     * @cfg {ajax.config.ajax}
+     * @protected
+     */
+    "ajax": AJAX
+  }, module.config());
 });
 
 /**
@@ -61,55 +61,57 @@ define('troopjs-ajax/error',[ "mu-error/factory" ], function (Factory) {
 
   return Factory("AjaxError");
 });
+
 /**
  * @license MIT http://troopjs.mit-license.org/
  */
 define('troopjs-ajax/service',[
-	"troopjs-hub/component",
-	"./config",
-	"./error",
-	"jquery",
-	"mu-merge/main"
+  "troopjs-hub/component",
+  "./config",
+  "./error",
+  "jquery",
+  "mu-merge/main"
 ], function (Emitter, config, AjaxError, $, merge) {
-	
+  
 
-	/**
-	 * Provides ajax as a service
-	 * @class ajax.service
-	 * @extend hub.component
-	 * @mixin ajax.config
-	 * @alias service.ajax
-	 */
+  /**
+   * Provides ajax as a service
+   * @class ajax.service
+   * @extend hub.component
+   * @mixin ajax.config
+   * @alias service.ajax
+   */
 
-	var SETTINGS = config.ajax.settings;
+  var UNDEFINED;
+  var SETTINGS = config.ajax.settings;
 
-	/**
-	 * @method constructor
-	 */
-	return Emitter.extend({
-		"displayName" : "ajax/service",
+  /**
+   * @method constructor
+   */
+  return Emitter.extend({
+    "displayName": "ajax/service",
 
-		/**
-		 * The ajax event
-		 * @event hub/ajax
-		 * @param {Object} settings Ajax settings
-		 */
+    /**
+     * The ajax event
+     * @event hub/ajax
+     * @param {Object} settings Ajax settings
+     */
 
-		/**
-		 * Make ajax request
-		 * @handler
-		 * @inheritdoc #event-hub/ajax
-		 */
-		"hub/ajax" : function ajax(settings) {
-			return $
-				.ajax(merge.call(SETTINGS.call(this), settings))
-				.fail(function (jqXHR, textStatus, errorThrown) {
-					return new AjaxError(errorThrown);
-				});
-		}
-	});
+    /**
+     * Make ajax request
+     * @handler
+     * @inheritdoc #event-hub/ajax
+     */
+    "hub/ajax": function (settings) {
+      return $
+        .ajax(merge.call(SETTINGS.call(this), settings))
+        .then(UNDEFINED, function (jqXHR, textStatus, errorThrown) {
+          return new AjaxError(errorThrown);
+        });
+    }
+  });
 });
 
 define(['troopjs-ajax/version'], function (version) {
-	return version;
+  return version;
 });
